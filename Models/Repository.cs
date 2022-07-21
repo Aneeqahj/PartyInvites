09709e28d7bcbf9f6947af55a48c480d9e923604
unknown <aneeqahjones2@gmail.com>
2022-07-21 10:43:15 +0200

    Functional RSVP Form

---
+++ Models/Repository.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace PartyInvites2.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get { return responses; }
+        }
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
